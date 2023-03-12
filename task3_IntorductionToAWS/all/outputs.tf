3c4aabd8a1ab198c2e95f4134efef220df8d643d
Borys Kondrashov <95252258+Tuburni@users.noreply.github.com>
2023-03-12 20:03:48 +0200

    Delete task3_IntorductionToAWS directory

--- task3_IntorductionToAWS/all/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_public_ip" {                                  # Define a Terraform output variable called "instance_public_ip"
-  description = "Public IP of EC2 instance"                    # Add a description to the output variable
-  value       = aws_instance.moodle_server_instance.public_ip  # Set the value of the output variable to the public IP address of the EC2 instance
-}
